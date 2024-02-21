a97df6c63cb9e59740ff7e1736ddf7713b0aed0a
meyersa <88554476+meyersa@users.noreply.github.com>
2024-02-21 16:40:16 -0500

    Im dumb

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,4 @@ COPY . .
 
 RUN mkdir /results
 
-RUN python3 sortingAlg.py "inp-1000k/infile1.dat" "results/output1" 1000000 0 1 1
-RUN python3 sortingAlg.py "inp-1000k/infile2.dat" "results/output2" 1000000 0 1 1
-RUN python3 sortingAlg.py "inp-1000k/infile3.dat" "results/output3" 1000000 0 1 1
-RUN python3 sortingAlg.py "inp-1000k/infile4.dat" "results/output4" 1000000 0 1 1
-
-RUN curl -X POST -H "Content-Type: application/json" -d '{"content":null,"embeds":[{"title":"CPS542 PyAlgComp","description":"Finished running algorithms","color":null}],"attachments":[]}' $WEBHOOK_URL
+ENTRYPOINT [ "entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Run sorting algorithms
+python3 sortingAlg.py "inp-1000k/infile1.dat" "results/output1" 1000000 0 1 1 &
+python3 sortingAlg.py "inp-1000k/infile2.dat" "results/output2" 1000000 0 1 1 &
+python3 sortingAlg.py "inp-1000k/infile3.dat" "results/output3" 1000000 0 1 1 &
+python3 sortingAlg.py "inp-1000k/infile4.dat" "results/output4" 1000000 0 1 1 &
+
+# Wait for all sorting algorithms to finish
+wait
+
+# Trigger curl command
+curl -X POST -H "Content-Type: application/json" -d '{"content":null,"embeds":[{"title":"CPS542 PyAlgComp","description":"Finished running algorithms","color":null}],"attachments":[]}' $WEBHOOK_URL
