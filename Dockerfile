dba74fb276c0042d1f89ce56b14cbb5cad3dfbeb
meyersa <88554476+meyersa@users.noreply.github.com>
2024-02-21 22:13:03 -0500

    Container fixes

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,10 @@ FROM python:3.9.18
 
 COPY . .
 
-RUN mkdir /results
+# RUN mkdir /results
 
 RUN chmod +x entrypoint.sh
 
-ENTRYPOINT [ "entrypoint.sh" ]
+RUN ls -a 
+
+ENTRYPOINT [ "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 # Run sorting algorithms
-python3 sortingAlg.py "inp-1000k/infile1.dat" "results/output1" 1000000 0 1 1 &
-python3 sortingAlg.py "inp-1000k/infile2.dat" "results/output2" 1000000 0 1 1 &
-python3 sortingAlg.py "inp-1000k/infile3.dat" "results/output3" 1000000 0 1 1 &
-python3 sortingAlg.py "inp-1000k/infile4.dat" "results/output4" 1000000 0 1 1 &
+python3 -u sortingAlg.py "inp-1000k/infile1.dat" "results/output1" 1000000 0 1 1 1
+wait
 
-# Wait for all sorting algorithms to finish
+python3 -u sortingAlg.py "inp-1000k/infile2.dat" "results/output2" 1000000 0 1 1 1
+wait
+
+python3 -u sortingAlg.py "inp-1000k/infile3.dat" "results/output3" 1000000 0 1 1 1
+wait
+
+python3 -u sortingAlg.py "inp-1000k/infile4.dat" "results/output4" 1000000 0 1 1 1
 wait
 
 # Trigger curl command
